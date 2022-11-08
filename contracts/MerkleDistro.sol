372fdf19cc1c5537f264e4b1ec0069643565efe4
cristovaoth <cristovao.honorato@gmail.com>
2022-11-08 02:29:08 +0100

    Reintroduce SafeERC20 for MerkleDistro

--- contracts/MerkleDistro.sol
+++ contracts/MerkleDistro.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import "./MerkleProof.sol";
 
 contract MerkleDistro is Ownable {
+  using SafeERC20 for IERC20;
+
   event Claimed(address indexed claimer, uint256 amount);
 
   address public immutable token;
@@ -19,28 +22,31 @@ contract MerkleDistro is Ownable {
     transferOwnership(owner);
   }
 
-  function claim(bytes32[] calldata merkleProof, uint256 allocated) external {
-    verify(merkleProof, allocated);
+  function claim(
+    bytes32[] calldata merkleProof,
+    uint256 amountAllocated
+  ) external {
+    verify(merkleProof, amountAllocated);
 
     address beneficiary = msg.sender;
-    uint256 amount = calculate(beneficiary, allocated);
+    uint256 amountToClaim = calculate(beneficiary, amountAllocated);
 
-    claimed[beneficiary] += amount;
-    require(
-      IERC20(token).transfer(beneficiary, amount),
-      "Token Transfer Failed"
-    );
+    claimed[beneficiary] += amountToClaim;
+    IERC20(token).safeTransfer(beneficiary, amountToClaim);
 
-    emit Claimed(beneficiary, amount);
+    emit Claimed(beneficiary, amountToClaim);
   }
 
-  function verify(bytes32[] calldata proof, uint256 allocated) internal view {
+  function verify(
+    bytes32[] calldata proof,
+    uint256 amountAllocated
+  ) internal view {
     // Computing proof using leaf double hashing
     // https://flawed.net.nz/2018/02/21/attacking-merkle-trees-with-a-second-preimage-attack/
 
     bytes32 root = merkleRoot;
     bytes32 leaf = keccak256(
-      bytes.concat(keccak256(abi.encode(msg.sender, allocated)))
+      bytes.concat(keccak256(abi.encode(msg.sender, amountAllocated)))
     );
 
     require(MerkleProof.verify(proof, root, leaf), "Invalid Allocation Proof");
