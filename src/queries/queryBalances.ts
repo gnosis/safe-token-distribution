98d1085626cb007b3a84be388d5cfb8874d74a5b
cristovaoth <cristovao.honorato@gmail.com>
2022-11-10 00:21:08 +0100

    Adding temporary balance data

--- src/queries/queryBalances.ts
+++ src/queries/queryBalances.ts
@@ -3,8 +3,10 @@ import { request, gql } from "graphql-request";
 
 type User = {
   id: string;
+  gno: string;
   mgno: string;
   lgno: string;
+  sgno: string;
 };
 type Result = { [key: string]: BigNumber };
 
@@ -17,50 +19,53 @@ export async function queryBalancesMainnet(block: number): Promise<Result> {
     count: 1000,
   });
 
-  return (
-    result.users
-      .map((user: User) => ({
-        id: user.id,
-        balance: BigNumber.from(user.lgno).add(user.mgno),
-      }))
-      //.filter(({ balance }: { balance: BigNumber }) => balance.gt(0))
-      .reduce((prev: Result, next: any) => {
-        return {
-          ...prev,
-          [next.id]: next.balance,
-        };
-      }, {})
-  );
+  return result.users
+    .map((user: User) => ({
+      id: user.id,
+      balance: BigNumber.from(user.gno)
+        .add(user.mgno)
+        .add(user.lgno)
+        .add(user.sgno),
+    }))
+    .filter(({ balance }: { balance: BigNumber }) => balance.gt(0))
+    .reduce((prev: Result, next: any) => {
+      return {
+        ...prev,
+        [next.id]: next.balance,
+      };
+    }, {});
 }
 
 export async function queryBalancesGC(block: number): Promise<Result> {
   const result = await request(gcEndpoint, gcQuery, { block, count: 1000 });
 
-  return (
-    result.users
-      .map((user: User) => ({
-        id: user.id,
-        balance: BigNumber.from(user.lgno).add(user.mgno),
-      }))
-      //.filter(({ balance }: { balance: BigNumber }) => balance.gt(0))
-      .reduce((prev: Result, next: any) => {
-        return {
-          ...prev,
-          [next.id]: next.balance,
-        };
-      }, {})
-  );
+  return result.users
+    .map((user: User) => ({
+      id: user.id,
+      balance: BigNumber.from(user.gno)
+        .add(user.mgno)
+        .add(user.lgno)
+        .add(user.sgno),
+    }))
+    .filter(({ balance }: { balance: BigNumber }) => balance.gt(0))
+    .reduce((prev: Result, next: any) => {
+      return {
+        ...prev,
+        [next.id]: next.balance,
+      };
+    }, {});
 }
 
 const mainnetEndpoint =
   "https://api.thegraph.com/subgraphs/name/jfschwarz/gno-voting-power-eth";
 const mainnetQuery = gql`
   query ($block: Int, $count: Int, $lastId: ID) {
-    # this one is coming from uniswap-v2
-    users(block: { number: $block }, first: $count, where: { lgno_not: "0" }) {
+    users(block: { number: $block }, first: $count) {
       id
-      lgno
+      gno
       mgno
+      lgno
+      sgno
     }
   }
 `;
@@ -70,11 +75,12 @@ const gcEndpoint =
 
 const gcQuery = gql`
   query ($block: Int, $count: Int, $lastId: ID) {
-    # this one is coming from uniswap-v2
-    users(first: $count, where: { lgno_not: "0" }) {
+    users(first: $count) {
       id
-      lgno
+      gno
       mgno
+      lgno
+      sgno
     }
   }
 `;
