d3ced78bb04f911c99da60accca9885ab649be7e
cristovaoth <cristovao.honorato@gmail.com>
2022-11-09 23:56:21 +0100

    Create harvest task that composes the remainder of snapshot tasks

---
+++ src/tasks/harvest.ts
@@ -0,0 +1,16 @@
+import { task, types } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+task("harvest", "")
+  .addOptionalParam(
+    "lazy",
+    "Don't recalculate if result is found on disk",
+    true,
+    types.boolean,
+  )
+  .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+    await hre.run("snapshot:schedule", { lazy: taskArgs.lazy });
+    await hre.run("snapshot:blocks", { lazy: taskArgs.lazy });
+    //await hre.run("snapshot:balances");
+    await hre.run("snapshot:totalvested", { lazy: taskArgs.lazy });
+  });
