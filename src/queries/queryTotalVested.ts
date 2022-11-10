fc79ee7e741ecb6e2c1eaf11ea7f4d4b2dd5cccc
cristovaoth <cristovao.honorato@gmail.com>
2022-11-10 21:20:27 +0100

    Use eth_call to query archive node at certain height

--- src/queries/queryTotalVested.ts
+++ src/queries/queryTotalVested.ts
@@ -1,4 +1,3 @@
-import { assert } from "console";
 import { BigNumber } from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { VestingPool__factory } from "../../typechain";
@@ -9,48 +8,22 @@ export async function queryTotalVested(
   vestingAddress: string,
   vestingId: string,
 ): Promise<BigNumber> {
-  await fork(hre, blockNumber);
-
-  const vestingPool = VestingPool__factory.connect(
-    vestingAddress,
-    hre.ethers.provider,
+  const provider = new hre.ethers.providers.JsonRpcProvider(
+    `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
   );
 
-  const { vestedAmount } = await vestingPool.calculateVestedAmount(vestingId);
+  const iface = VestingPool__factory.createInterface();
 
-  await forkReset(hre);
+  const data = await iface.encodeFunctionData("calculateVestedAmount", [
+    vestingId,
+  ]);
+
+  const result = await provider.call({ to: vestingAddress, data }, blockNumber);
+
+  const { vestedAmount } = iface.decodeFunctionResult(
+    "calculateVestedAmount",
+    result,
+  );
 
   return vestedAmount;
-}
-
-async function fork(hre: HardhatRuntimeEnvironment, blockNumber: number) {
-  assert(
-    hre.network.config.chainId === 31337,
-    "Query should run in hardhat network mode",
-  );
-
-  const { ALCHEMY_KEY } = process.env;
-  await hre.network.provider.request({
-    method: "hardhat_reset",
-    params: [
-      {
-        forking: {
-          jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
-          blockNumber,
-        },
-      },
-    ],
-  });
-}
-
-async function forkReset(hre: HardhatRuntimeEnvironment) {
-  assert(
-    hre.network.config.chainId === 31337,
-    "Query should run in hardhat fork mode",
-  );
-
-  await hre.network.provider.request({
-    method: "hardhat_reset",
-    params: [],
-  });
 }
