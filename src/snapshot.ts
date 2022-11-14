673ab99f2f475f684cf14ba40f08a1dd58bf5b0c
cristovaoth <cristovao.honorato@gmail.com>
2022-11-14 18:10:21 +0100

    Tweek some details that have a big impact in fs performance

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -13,13 +13,25 @@ export function sum(snapshot: Snapshot): BigNumber {
 }
 
 export function merge(s1: Snapshot, s2: Snapshot): Snapshot {
-  return Object.keys(s1).reduce(
-    (result, key) => ({
-      ...result,
-      [key]: s1[key].add(s2[key] || 0),
-    }),
-    s2,
-  );
+  // !!this is MUCH slower!!
+  // return Object.keys(s1).reduce(
+  //   (result, key) => ({
+  //     ...result,
+  //     [key]: s1[key].add(s2[key] || 0),
+  //   }),
+  //   s2,
+  // );
+
+  //this is faster
+  const [dest, src] =
+    Object.keys(s1).length > Object.keys(s2).length
+      ? [{ ...s1 }, s2]
+      : [{ ...s2 }, s1];
+
+  for (const key in src) {
+    dest[key] = src[key].add(dest[key] || 0);
+  }
+  return dest;
 }
 
 export function load(filePath: string): Snapshot | null {
@@ -43,21 +55,36 @@ type StringMap = {
 };
 
 function valuesToBigNumber(map: StringMap): Snapshot {
-  return Object.keys(map).reduce(
-    (prev, key) => ({
-      ...prev,
-      [key]: BigNumber.from(map[key]),
-    }),
-    {},
-  );
+  // !!this is (catastrophically) MUCH slower!!
+  // return Object.keys(map).reduce(
+  //   (prev, key) => ({
+  //     ...prev,
+  //     [key]: BigNumber.from(map[key]),
+  //   }),
+  //   {},
+  // );
+  // this is faster
+  const result: Snapshot = {};
+  for (const key in map) {
+    result[key] = BigNumber.from(map[key]);
+  }
+  return result;
 }
 
 function valuesToString(map: Snapshot): StringMap {
-  return Object.keys(map).reduce(
-    (prev, key) => ({
-      ...prev,
-      [key]: map[key].toString(),
-    }),
-    {},
-  );
+  // !!this is MUCH slower!!
+  // return Object.keys(map).reduce(
+  //   (prev, key) => ({
+  //     ...prev,
+  //     [key]: map[key].toString(),
+  //   }),
+  //   {},
+  // );
+
+  // this is faster
+  const result: StringMap = {};
+  for (const key in map) {
+    result[key] = map[key].toString();
+  }
+  return result;
 }
