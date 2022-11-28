80aadac2a2f639c03c25db240b0ba1f1c850572e
samepant <sampanter@gmail.com>
2022-11-28 17:18:15 -0500

    add allocation status message

--- claim-gui/src/components/AllocationInfo/index.tsx
+++ claim-gui/src/components/AllocationInfo/index.tsx
@@ -1,4 +1,6 @@
+import { BigNumber } from "ethers";
 import { useAccount } from "wagmi";
+import clsx from "clsx";
 
 import { useAllocation } from "../../AllocationProvider";
 import { useAmountClaimed } from "../../hooks";
@@ -10,19 +12,40 @@ const AllocationInfo: React.FC = () => {
   const { address } = useAccount();
   const allocation = useAllocation();
   const { amountClaimed } = useAmountClaimed(address);
+
   return (
     <Card>
       {allocation && (
-        <dl className={classes.container}>
-          <div className={classes.item}>
-            <dt className={classes.label}>Claimed</dt>
-            <dd className={classes.data}>{amountClaimed.toString()}</dd>
-          </div>
-          <div className={classes.item}>
-            <dt className={classes.label}>Allocated</dt>
-            <dd className={classes.data}>{allocation.amount.toString()}</dd>
-          </div>
-        </dl>
+        <div>
+          <dl className={classes.container}>
+            <div className={classes.item}>
+              <dt className={classes.label}>Claimed</dt>
+              <dd className={classes.data}>{amountClaimed.toString()}</dd>
+            </div>
+            <div className={classes.item}>
+              <dt className={classes.label}>Allocated</dt>
+              <dd className={classes.data}>{allocation.amount.toString()}</dd>
+            </div>
+          </dl>
+
+          {allocation.amount > amountClaimed && (
+            <div className={clsx(classes.status, classes.available)}>
+              You have SAFE tokens to claim
+            </div>
+          )}
+
+          {allocation.amount === amountClaimed && (
+            <div className={clsx(classes.status, classes.fullClaim)}>
+              You've claimed all available SAFE tokens
+            </div>
+          )}
+
+          {allocation.amount === BigNumber.from(0) && (
+            <div className={clsx(classes.status, classes.notAvailable)}>
+              You have no SAFE tokens to claim
+            </div>
+          )}
+        </div>
       )}
 
       {address && !allocation && <p>{"No Allocation"}</p>}
