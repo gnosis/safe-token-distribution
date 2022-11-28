f043b9f52a6dfea9e4e062e0a5167e5ca758184b
samepant <sampanter@gmail.com>
2022-11-28 16:49:24 -0500

    action button shows connect

--- claim-gui/src/components/MainActionButton/index.tsx
+++ claim-gui/src/components/MainActionButton/index.tsx
@@ -4,25 +4,46 @@ import { useAllocation } from "../../AllocationProvider";
 import ClaimButton from "./ClaimButton";
 import { useAmountClaimed } from "../../hooks";
 import Card from "../Card";
+import Button from "../Button";
+import ConnectModal from "../ConnectModal";
+import { useEffect, useState } from "react";
 
 const MainActionButton: React.FC = () => {
   const { address } = useAccount();
   const allocation = useAllocation();
-  const {refetch} = useAmountClaimed(address)
+  const { refetch } = useAmountClaimed(address);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (address) {
+      setShowModal(false);
+    }
+  }, [address]);
 
   return (
     <Card>
-      {address && allocation && (
-        <ClaimButton
-          {...allocation}
-          onSuccess={() => {
-            alert(`Claimed ${allocation.amount}`);
-            refetch?.(address);
-          }}
-          onError={(err) => {
-            alert(`Error ${err}`);
-          }}
-        />
+      {showModal && <ConnectModal />}
+      {address ? (
+        <>
+          {allocation && (
+            <ClaimButton
+              {...allocation}
+              onSuccess={() => {
+                alert(`Claimed ${allocation.amount}`);
+                refetch?.(address);
+              }}
+              onError={(err) => {
+                alert(`Error ${err}`);
+              }}
+            />
+          )}
+        </>
+      ) : (
+        <>
+          <Button primary onClick={() => setShowModal(true)}>
+            Connect
+          </Button>
+        </>
       )}
     </Card>
   );
