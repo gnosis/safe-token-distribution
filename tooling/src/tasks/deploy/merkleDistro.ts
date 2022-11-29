0e688e5e7bc4376e18bec88cfda01abcc4f9cada
cristovaoth <cristovao.honorato@gmail.com>
2022-11-29 13:04:17 +0100

    Harmonize names

--- tooling/src/tasks/deploy/merkleDistro.ts
+++ tooling/src/tasks/deploy/merkleDistro.ts
@@ -1,21 +1,6 @@
-import type { MerkleDistro, MerkleDistro__factory } from "../../typechain";
+import type { MerkleDistro, MerkleDistro__factory } from "../../../typechain";
 import { Signer } from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-
-// import { task } from "hardhat/config";
-// import type { TaskArguments } from "hardhat/types";
-
-// task("deploy:MerkleDistro")
-//   .addParam("greeting", "Say hello, be nice")
-//   .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-//     const signers: SignerWithAddress[] = await ethers.getSigners();
-//     const greeterFactory = await ethers.getContractFactory("Greeter");
-//     const greeter = await greeterFactory
-//       .connect(signers[0])
-//       .deploy(taskArguments.greeting);
-//     await greeter.deployed();
-//     console.log("Greeter deployed to: ", greeter.address);
-//   });
 
 export async function deployMerkleDistro(
   hre: HardhatRuntimeEnvironment,
