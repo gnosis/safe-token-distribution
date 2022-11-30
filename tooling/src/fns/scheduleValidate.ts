68fc3fc4f1312b05f783660f25f7965e8383c1fc
cristovaoth <cristovao.honorato@gmail.com>
2022-11-30 23:36:29 +0100

    Consolidate schedule validate deep/shallow in a single function

---
+++ tooling/src/fns/scheduleValidate.ts
@@ -0,0 +1,44 @@
+import { Provider } from "@ethersproject/providers";
+import { Schedule } from "../persistence";
+
+import { Interval } from "../types";
+
+export default async function (
+  intervals: Interval[],
+  schedule: Schedule,
+  deep: boolean,
+  providers: { mainnet: Provider; gc: Provider },
+  log?: (l: string) => void,
+) {
+  for (let i = 0; i < schedule.length; i++) {
+    const entry = schedule[i];
+    const interval = intervals[i];
+
+    const [timestampMainnet, timestampGC] = deep
+      ? await Promise.all([
+          providers.mainnet
+            .getBlock(entry.mainnet.blockNumber)
+            .then(({ timestamp }) => timestamp),
+          providers.gc
+            .getBlock(entry.gc.blockNumber)
+            .then(({ timestamp }) => timestamp),
+        ])
+      : [entry.mainnet.timestamp, entry.gc.timestamp];
+
+    if (
+      !(interval.left <= timestampMainnet && timestampMainnet <= interval.right)
+    ) {
+      throw new Error(
+        `Mainnet block ${entry.mainnet.blockNumber} in schedule at position ${i} does not match interval`,
+      );
+    }
+
+    if (!(interval.left <= timestampGC && timestampGC <= interval.right)) {
+      throw new Error(
+        `GC block ${entry.gc.blockNumber} in schedule at position ${i} does not match interval`,
+      );
+    }
+
+    log?.(`${i}: OK`);
+  }
+}
