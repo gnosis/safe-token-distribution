25fa69f7ca9ec0ebc98548686eebc3aeee9e380f
samepant <sampanter@gmail.com>
2022-11-30 16:12:05 -0500

    vesting contract read update on new block

--- claim-gui/src/components/VestingInfo/index.tsx
+++ claim-gui/src/components/VestingInfo/index.tsx
@@ -1,6 +1,6 @@
 import { BigNumber } from "ethers";
 import { formatUnits } from "ethers/lib/utils.js";
-import { useBlockNumber, useContractReads } from "wagmi";
+import { useBlockNumber, useContractReads, useContractRead } from "wagmi";
 import safeTokenContract from "../../utils/SafeTokenContract";
 
 import vestingContract from "../../utils/vestingContract";
@@ -9,25 +9,11 @@ import classes from "./style.module.css";
 import VestingChart, { AllocationData } from "./VestingChart";
 
 const VestingInfo: React.FC = () => {
-  const blockRes = useBlockNumber({
-    chainId: 1,
-    onBlock(blockNumber) {
-      console.log("New block: ", blockNumber);
-    },
-  });
-
-  const { data, isError, isLoading } = useContractReads({
+  const staticRes = useContractReads({
     contracts: [
       {
         ...vestingContract,
         functionName: "vestings",
-        args: [
-          "0x12c1ee9f9b122fa7a0e7a6a733f6e07d30affb7fac1ca061325b11d9ba677382",
-        ],
-      },
-      {
-        ...vestingContract,
-        functionName: "calculateVestedAmount",
         args: [
           "0x12c1ee9f9b122fa7a0e7a6a733f6e07d30affb7fac1ca061325b11d9ba677382",
         ],
@@ -39,14 +25,27 @@ const VestingInfo: React.FC = () => {
     ],
   });
 
+  const vestingRes = useContractRead({
+    ...vestingContract,
+    functionName: "calculateVestedAmount",
+    args: [
+      "0x12c1ee9f9b122fa7a0e7a6a733f6e07d30affb7fac1ca061325b11d9ba677382",
+    ],
+    watch: true,
+  });
+
   // if data from contracts cannot be fetched, chart will show static
   // allocation diagram, with GnosisDAO having 15% of total supply
-  const totalSAFESupply = data ? data[2] : BigNumber.from("1000000000");
+  const totalSAFESupply = staticRes.data
+    ? staticRes.data[1]
+    : BigNumber.from("1000000000");
   const gnosisInitial = BigNumber.from("10011026319019003889472853");
-  const gnosisDAOTotal = data
-    ? data[0].amount.add(gnosisInitial)
+  const gnosisDAOTotal = staticRes.data
+    ? staticRes.data[0].amount.add(gnosisInitial)
     : BigNumber.from("150000000");
-  const vestedAmount = data ? data[1].vestedAmount : BigNumber.from(0);
+  const vestedAmount = vestingRes.data
+    ? vestingRes.data.vestedAmount
+    : BigNumber.from(0);
 
   const BNtoFloat = (BN: BigNumber, decimals: number): number => {
     return parseFloat(formatUnits(BN, decimals));
@@ -55,7 +54,7 @@ const VestingInfo: React.FC = () => {
   return (
     <div className={classes.vestingInfo}>
       <h1>GnosisDAO — SAFE Token</h1>
-      {!isLoading && (
+      {!staticRes.isLoading && !vestingRes.isLoading && (
         <VestingChart
           safeTokenSupply={BNtoFloat(totalSAFESupply, 18)}
           gnosisDaoAllocation={BNtoFloat(gnosisDAOTotal, 18)}
@@ -63,7 +62,7 @@ const VestingInfo: React.FC = () => {
         />
       )}
       <ConnectionStatus
-        isError={blockRes.isError}
+        isError={vestingRes.isError}
         className={classes.statusContainer}
       />
     </div>
