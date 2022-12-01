3f65bb427127b6f34ff63ccd67603c8da4108357
samepant <sampanter@gmail.com>
2022-12-01 15:46:38 -0500

    obscure data until wallet connected

--- claim-gui/src/components/SafeTag/index.tsx
+++ claim-gui/src/components/SafeTag/index.tsx
@@ -3,7 +3,7 @@ import classes from "./style.module.css";
 const SafeTag: React.FC = () => {
   return (
     <span className={classes.tag}>
-      <img src="/Safe_Logos_Symbol_Black.svg" />
+      <img src="/Safe_Logos_Symbol_Black.svg" alt="Safe Logo" />
       <span className={classes.name}>Safe</span>
     </span>
   );
