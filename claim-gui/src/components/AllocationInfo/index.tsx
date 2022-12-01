85ba2bcb8ff70d23a77f8de8640a86ec55f507d8
samepant <sampanter@gmail.com>
2022-12-01 15:46:38 -0500

    obscure data until wallet connected

--- claim-gui/src/components/AllocationInfo/index.tsx
+++ claim-gui/src/components/AllocationInfo/index.tsx
@@ -18,40 +18,58 @@ const AllocationInfo: React.FC = () => {
   return (
     <Card>
       <VestingInfo />
-      {allocation && (
-        <div>
-          <dl className={classes.container}>
-            <div className={classes.item}>
-              <dt className={classes.label}>Claimed</dt>
-              <dd className={classes.data}>{amountClaimed.toString()}</dd>
-            </div>
-            <div className={classes.item}>
-              <dt className={classes.label}>Allocated</dt>
-              <dd className={classes.data}>{allocation.amount.toString()}</dd>
-            </div>
-          </dl>
+      <div className={classes.allocation}>
+        <dl className={classes.container}>
+          <div className={classes.item}>
+            <dt className={classes.label}>Claimed</dt>
+            <dd className={classes.data}>{amountClaimed.toString()}</dd>
+          </div>
+          <div className={classes.item}>
+            <dt className={classes.label}>Allocated</dt>
+            <dd className={classes.data}>
+              {allocation ? allocation.amount.toString() : "0"}
+            </dd>
+          </div>
+        </dl>
 
-          {allocation.amount > amountClaimed && (
-            <div className={clsx(classes.status, classes.available)}>
+        {allocation && allocation.amount > amountClaimed && (
+          <div className={clsx(classes.status, classes.available)}>
+            <p>
               You have <SafeTag /> tokens to claim
-            </div>
-          )}
+            </p>
+          </div>
+        )}
 
-          {allocation.amount === amountClaimed && (
-            <div className={clsx(classes.status, classes.fullClaim)}>
+        {allocation && allocation.amount === amountClaimed && (
+          <div className={clsx(classes.status, classes.fullClaim)}>
+            <p>
               You've claimed all available <SafeTag /> tokens
-            </div>
-          )}
+            </p>
+          </div>
+        )}
 
-          {allocation.amount === BigNumber.from(0) && (
-            <div className={clsx(classes.status, classes.notAvailable)}>
+        {!allocation && address && (
+          <div className={clsx(classes.status, classes.notAvailable)}>
+            <p>
               You have no <SafeTag /> tokens to claim
-            </div>
-          )}
-        </div>
-      )}
+            </p>
+          </div>
+        )}
 
-      {address && !allocation && <p>{"No Allocation"}</p>}
+        {!address && (
+          <div className={clsx(classes.status, classes.available)}>
+            <p>
+              You have <SafeTag /> tokens to claim
+            </p>
+          </div>
+        )}
+
+        {!address && (
+          <div className={classes.obscure}>
+            <p>Connect a wallet to view your claim.</p>
+          </div>
+        )}
+      </div>
     </Card>
   );
 };
