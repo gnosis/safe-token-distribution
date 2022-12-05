66edbf4a9e6daad324c4fb4c8bac795531d725ac
cristovaoth <cristovao.honorato@gmail.com>
2022-12-05 12:09:08 +0100

    Hoist calculation that was buried inside a query* function into task level

--- tooling/src/fns/snapshotWithout.ts
+++ tooling/src/fns/snapshotWithout.ts
@@ -1,11 +1,14 @@
+import { getAddress } from "ethers/lib/utils";
 import { Snapshot } from "../types";
 
 export default function snapshotWithout(
   snapshot: Snapshot,
   toRemove: string[],
 ): Snapshot {
+  toRemove = toRemove.map((address) => getAddress(address));
+
   const addresses = Object.keys(snapshot).filter(
-    (address) => !toRemove.includes(address),
+    (address) => !toRemove.includes(getAddress(address)),
   );
 
   const nextSnapshot: Snapshot = {};
