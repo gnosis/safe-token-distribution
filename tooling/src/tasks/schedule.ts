440630ca9543ff862799acaddfb070329a7a8fe7
cristovaoth <cristovao.honorato@gmail.com>
2022-12-09 00:40:38 +0100

    Remove unnecessary task arguments

--- tooling/src/tasks/schedule.ts
+++ tooling/src/tasks/schedule.ts
@@ -21,92 +21,64 @@ import { loadSchedule, saveSchedule } from "../persistence";
 task(
   "schedule:expand",
   "For any past vesting interval not yet persisted, finds random blocks writes to schedule.json",
-)
-  .addOptionalParam(
-    "inception",
-    "vesting start block",
-    VESTING_CREATION_BLOCK,
-    types.int,
-  )
-  .addOptionalParam(
-    "frequency",
-    "distribution snapshot frequency in minutes",
+).setAction(async (_, hre: HardhatRuntimeEnvironment) => {
+  const log = (text: string) => console.info(`Task schedule:expand -> ${text}`);
+  const providers = getProviders(hre);
+
+  log("Starting...");
+
+  const intervals = intervalsGenerate(
+    await providers.mainnet.getBlock(VESTING_CREATION_BLOCK),
     SNAPSHOT_FREQUENCY_IN_MINUTES,
-    types.int,
-  )
-  .setAction(
-    async ({ inception, frequency }, hre: HardhatRuntimeEnvironment) => {
-      const log = (text: string) =>
-        console.info(`Task schedule:expand -> ${text}`);
-      const providers = getProviders(hre);
+  ).filter(isPastInterval);
 
-      log("Starting...");
+  const prevSchedule = loadSchedule();
+  assert(prevSchedule.length <= intervals.length);
 
-      const intervals = intervalsGenerate(
-        await providers.mainnet.getBlock(inception),
-        frequency,
-      ).filter(isPastInterval);
+  if (prevSchedule.length == intervals.length) {
+    log("Already up to date");
+    return prevSchedule;
+  }
 
-      const prevSchedule = loadSchedule();
-      assert(prevSchedule.length <= intervals.length);
+  log(`Inserting ${intervals.length - prevSchedule.length} new entries`);
 
-      if (prevSchedule.length == intervals.length) {
-        log("Already up to date");
-        return prevSchedule;
-      }
-
-      log(`Inserting ${intervals.length - prevSchedule.length} new entries`);
-
-      const nextMainnetEntries = await intervalsToBlocks(
-        intervals.slice(prevSchedule.length),
-        providers.mainnet,
-      );
-
-      let nextSchedule = [...prevSchedule];
-      for (const mainnetEntry of nextMainnetEntries) {
-        log(
-          `Finding a block in GC that matches mainnet ${mainnetEntry.blockNumber}`,
-        );
-
-        const blockGC = await queryClosestBlock(
-          mainnetEntry.timestamp,
-          providers.gnosis,
-        );
-        nextSchedule = [
-          ...nextSchedule,
-          {
-            mainnet: mainnetEntry,
-            gnosis: {
-              timestamp: blockGC.timestamp,
-              blockNumber: blockGC.number,
-            },
-          },
-        ];
-      }
-
-      saveSchedule(nextSchedule);
-      log("Done");
-
-      return nextSchedule;
-    },
+  const nextMainnetEntries = await intervalsToBlocks(
+    intervals.slice(prevSchedule.length),
+    providers.mainnet,
   );
+
+  let nextSchedule = [...prevSchedule];
+  for (const mainnetEntry of nextMainnetEntries) {
+    log(
+      `Finding a block in GC that matches mainnet ${mainnetEntry.blockNumber}`,
+    );
+
+    const blockGC = await queryClosestBlock(
+      mainnetEntry.timestamp,
+      providers.gnosis,
+    );
+    nextSchedule = [
+      ...nextSchedule,
+      {
+        mainnet: mainnetEntry,
+        gnosis: {
+          timestamp: blockGC.timestamp,
+          blockNumber: blockGC.number,
+        },
+      },
+    ];
+  }
+
+  saveSchedule(nextSchedule);
+  log("Done");
+
+  return nextSchedule;
+});
 
 task(
   "schedule:validate",
   "Validates the schedule written to disk, and ensures it matches interval guidelines",
 )
-  .addOptionalParam(
-    "inception",
-    "vesting start block",
-    VESTING_CREATION_BLOCK,
-    types.int,
-  )
-  .addOptionalParam(
-    "frequency",
-    "distribution snapshot frequency in minutes",
-    SNAPSHOT_FREQUENCY_IN_MINUTES,
-    types.int,
-  )
   .addOptionalParam(
     "deep",
     "refetch block info, and do exhaustive checks",
@@ -119,49 +91,44 @@ task(
     false,
     types.boolean,
   )
-  .setAction(
-    async (
-      { inception, frequency, deep, frozen },
-      hre: HardhatRuntimeEnvironment,
-    ) => {
-      const log = (text: string) =>
-        console.info(`Task schedule:validate -> ${text}`);
-      log("Starting...");
+  .setAction(async ({ deep, frozen }, hre: HardhatRuntimeEnvironment) => {
+    const log = (text: string) =>
+      console.info(`Task schedule:validate -> ${text}`);
+    log("Starting...");
 
-      const providers = getProviders(hre);
+    const providers = getProviders(hre);
 
-      const intervals = intervalsGenerate(
-        await providers.mainnet.getBlock(inception),
-        frequency,
-      ).filter(isPastInterval);
+    const intervals = intervalsGenerate(
+      await providers.mainnet.getBlock(VESTING_CREATION_BLOCK),
+      SNAPSHOT_FREQUENCY_IN_MINUTES,
+    ).filter(isPastInterval);
 
-      const schedule = loadSchedule();
-      log(
-        `Validating ${schedule.length} entries ${
-          deep ? "(with block metadata refetching)" : ""
-        }`,
+    const schedule = loadSchedule();
+    log(
+      `Validating ${schedule.length} entries ${
+        deep ? "(with block metadata refetching)" : ""
+      }`,
+    );
+
+    if (schedule.length > intervals.length) {
+      throw new Error("More schedule entries than past vesting intervals");
+    }
+
+    await scheduleValidate(
+      intervals,
+      schedule,
+      deep,
+      providers,
+      deep ? log : () => {},
+    );
+
+    if (frozen && schedule.length < intervals.length) {
+      throw new Error(
+        "One or more past vesting intervals don't yet have a matching entry in schedule.json",
       );
-
-      if (schedule.length > intervals.length) {
-        throw new Error("More schedule entries than past vesting intervals");
-      }
-
-      await scheduleValidate(
-        intervals,
-        schedule,
-        deep,
-        providers,
-        deep ? log : () => {},
-      );
-
-      if (frozen && schedule.length < intervals.length) {
-        throw new Error(
-          "One or more past vesting intervals don't yet have a matching entry in schedule.json",
-        );
-      }
-      console.log("schedule:validate Ok & Done");
-    },
-  );
+    }
+    console.log("schedule:validate Ok & Done");
+  });
 
 const isPastInterval = (interval: Interval) =>
   interval.right < getUnixTime(new Date());
