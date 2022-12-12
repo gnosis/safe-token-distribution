e37f4310366c58bcd71eb58277a6cf4ae245767b
cristovaoth <cristovao.honorato@gmail.com>
2022-12-12 13:01:52 +0100

    Include claim transaction progress feedback

--- claim-gui/src/components/MainActionButton/index.tsx
+++ claim-gui/src/components/MainActionButton/index.tsx
@@ -4,12 +4,14 @@ import { useAccount, useDisconnect } from "wagmi";
 import { useAllocation } from "../../hooks/AllocationProvider";
 
 import useAmountClaimed from "../../hooks/useAmountClaimed";
-import ClaimButton from "./ClaimButton";
+import ClaimButton, { ClaimStage } from "./ClaimButton";
 import Card from "../Card";
 import Button from "../Button";
 import ConnectModal from "../ConnectModal";
 
-const MainActionButton: React.FC = () => {
+const MainActionButton: React.FC<{
+  onProgress: (nextStage: ClaimStage) => void;
+}> = ({ onProgress }) => {
   const { address } = useAccount();
   const allocation = useAllocation();
   const amountClaimed = useAmountClaimed(address);
@@ -28,13 +30,9 @@ const MainActionButton: React.FC = () => {
         <>
           {allocation && allocation.amount.gt(amountClaimed) ? (
             <ClaimButton
-              {...allocation}
-              onSuccess={() => {
-                console.info("success");
-              }}
-              onError={(err) => {
-                console.log(err);
-              }}
+              proof={allocation.proof}
+              amount={allocation.amount}
+              onProgress={onProgress}
             />
           ) : (
             <Button primary onClick={() => disconnect()}>
