1f17664eec2c0ced4d4a4fa410e8998d459ecf40
cristovaoth <cristovao.honorato@gmail.com>
2022-12-12 15:44:36 +0100

    Extract status checks to a task

--- tooling/hardhat.config.ts
+++ tooling/hardhat.config.ts
@@ -2,6 +2,7 @@ import "./src/tasks/verify-distro";
 
 import "./src/tasks/schedule";
 import "./src/tasks/allocate";
+import "./src/tasks/status";
 import "./src/tasks/checkpoint";
 import "./src/tasks/propose";
 
