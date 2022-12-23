1d42dc3021b5db28b88b7af467beaa11eacb0b8b
cristovaoth <cristovao.honorato@gmail.com>
2022-12-23 23:31:06 +0000

    Remove unused function

--- tooling/src/fns/scheduleFind.ts
+++
@@ -1,6 +0,0 @@
-import { Schedule } from "../types";
-
-export default function scheduleFind(schedule: Schedule, blockNumber: number) {
-  const index = schedule.findIndex((entry) => blockNumber <= entry.mainnet);
-  return index === -1 ? null : schedule[index];
-}
