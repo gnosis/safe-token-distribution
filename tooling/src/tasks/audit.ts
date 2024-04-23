dc779511c21ff97f7dd82f85f755d8826b112e92
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-04-23 02:33:45 +0200

    More details on Audit

--- tooling/src/tasks/audit.ts
+++ tooling/src/tasks/audit.ts
@@ -2,12 +2,15 @@ import assert from "assert";
 import path from "path";
 import { readdirSync } from "fs";
 import { BigNumber } from "ethers";
+import { formatUnits, parseUnits } from "ethers/lib/utils";
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import { ALLOCATIONS_DIR, loadSnapshot } from "../persistence";
 import balancemapSum from "../fns/balancemapSum";
-import { formatUnits, parseUnits } from "ethers/lib/utils";
+
+import balancemapMerge from "../fns/balancemapMerge";
+import { BalanceMap } from "../types";
 
 task("audit", "Verifies the sum of all allocations snapshots").setAction(
   async (_, hre: HardhatRuntimeEnvironment) => {
@@ -29,32 +32,34 @@ task("audit", "Verifies the sum of all allocations snapshots").setAction(
 
     log("Starting");
 
+    let allocations: BalanceMap = {};
     let total = BigNumber.from(0);
 
     for (const fileName of readdirSync(ALLOCATIONS_DIR)) {
       const filePath = path.join(ALLOCATIONS_DIR, fileName);
       log(`Considering ${fileName}`);
-      const allocation = loadSnapshot(filePath);
-      assert(allocation);
-      total = total.add(balancemapSum(allocation));
+      const snapshot = loadSnapshot(filePath);
+      assert(snapshot);
+      total = total.add(balancemapSum(snapshot));
+      allocations = balancemapMerge(allocations, snapshot);
+    }
 
-      for (const key of Object.keys(allocation)) {
-        const value = allocation[key];
-        if (value.gt(bucket100K)) {
-          countBucket100K++;
-        } else if (value.gt(bucket10K)) {
-          countBucket10K++;
-        } else if (value.gt(bucket1K)) {
-          countBucket1K++;
-        } else if (value.gt(bucket100)) {
-          countBucket100++;
-        } else if (value.gt(bucket10)) {
-          countBucket10++;
-        } else if (value.gt(bucket1)) {
-          countBucket1++;
-        } else {
-          floor++;
-        }
+    for (const key of Object.keys(allocations)) {
+      const value = allocations[key];
+      if (value.gt(bucket100K)) {
+        countBucket100K++;
+      } else if (value.gt(bucket10K)) {
+        countBucket10K++;
+      } else if (value.gt(bucket1K)) {
+        countBucket1K++;
+      } else if (value.gt(bucket100)) {
+        countBucket100++;
+      } else if (value.gt(bucket10)) {
+        countBucket10++;
+      } else if (value.gt(bucket1)) {
+        countBucket1++;
+      } else {
+        floor++;
       }
     }
 
@@ -68,5 +73,13 @@ task("audit", "Verifies the sum of all allocations snapshots").setAction(
     log(`Accounts    + 10 SAFE ${countBucket10}`);
     log(`Accounts     + 1 SAFE ${countBucket1}`);
     log(`Accounts     < 1 SAFE ${floor}`);
+
+    const [first, second, third] = Object.entries(allocations).sort(
+      ([, v1], [, v2]) => (v1.gt(v2) ? -1 : 1),
+    );
+
+    log(`First  ${first[0]} ${formatUnits(first[1])}`);
+    log(`Second ${second[0]} ${formatUnits(second[1])}`);
+    log(`Third  ${third[0]} ${formatUnits(third[1])}`);
   },
 );
